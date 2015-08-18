b7239f6ed25d384efc35b269079857ab3ca2a2a8
Raphaël Tuor <raphael.tuor@gmail.com>
2015-08-18 15:42:23 +0200

    Project

---
+++ Assets/Scripts/enemyMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMove : MonoBehaviour {
+	
+	public int speedFactor = -1;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (Vector3.right * speedFactor * Time.deltaTime);
+	
+
+	}
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+		print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
+		print("Their relative velocity is " + collisionInfo.relativeVelocity);
+	}
+	
+}
