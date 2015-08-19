19baa031b618eca0219815134d0f511a58e0adef
Raphaël Tuor <raphael.tuor@gmail.com>
2015-08-19 22:48:38 +0200

    ViolonSlider added

--- Assets/Scripts/CheckKeysPressed.cs
+++ Assets/Scripts/CheckKeysPressed.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CheckKeysPressed : MonoBehaviour {
@@ -7,33 +8,53 @@ public class CheckKeysPressed : MonoBehaviour {
 	public GameObject enemy;
 	public float enemySpeedFactor;
 
+	public float difficulty;
+	
+	public GameObject violonSlider;
+
+
 	// Use this for initialization
 	void Start () {
 
-	
+		difficulty = 0.05f;
+
+		violonSlider = GameObject.FindGameObjectWithTag("ViolonSlider");
+
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		enemySpeedFactor = enemy.GetComponent<MovePosition>().speedFactor;
-}
+
+	}
 	//while note is in the box, check which key is pressed
 	void OnTriggerStay2D(Collider2D coll){
 		
 		// Black note + down = good
 		
-		if (Input.GetKeyUp ("down") && coll.gameObject.tag == "BlackNote") {
+		if (Input.GetKeyUp ("down") && coll.gameObject.tag == "BlackNote" && enemy.GetComponent<MovePosition>().speedFactor > 0.0f) {
 
-			enemy.GetComponent<MovePosition>().speedFactor = enemySpeedFactor -0.05f;
+			enemy.GetComponent<MovePosition>().speedFactor = enemySpeedFactor -difficulty;
+
+			violonSlider.GetComponent<Slider>().value += 5;
 
 		} else if(Input.GetKeyUp ("down") && coll.gameObject.tag == "WhiteNote") {
-			enemy.GetComponent<MovePosition>().speedFactor = enemySpeedFactor +0.05f;
+			enemy.GetComponent<MovePosition>().speedFactor = enemySpeedFactor +difficulty;
 
-		} else if (Input.GetKeyUp ("up") && coll.gameObject.tag == "WhiteNote") {
-			enemy.GetComponent<MovePosition>().speedFactor = enemySpeedFactor -0.05f;
+			violonSlider.GetComponent<Slider>().value -= 5;
+
+		} else if (Input.GetKeyUp ("up") && coll.gameObject.tag == "WhiteNote" && enemy.GetComponent<MovePosition>().speedFactor > 0.0f) {
+			enemy.GetComponent<MovePosition>().speedFactor = enemySpeedFactor -difficulty;
+
+			violonSlider.GetComponent<Slider>().value += 5;
+
 
 		} else if(Input.GetKeyUp ("up") && coll.gameObject.tag == "BlackNote") {
-			enemy.GetComponent<MovePosition>().speedFactor = enemySpeedFactor +0.05f;
+			enemy.GetComponent<MovePosition>().speedFactor = enemySpeedFactor +difficulty;
+
+			violonSlider.GetComponent<Slider>().value -= 5;
+
 
 		}
 	}
