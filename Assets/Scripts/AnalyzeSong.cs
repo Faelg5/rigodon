3a60698270ab36b383acc27b562a4b2259175369
Krakt <skapferer@sten.ch>
2015-08-20 18:55:54 +0200

    analyzeSong
    asd

--- Assets/Scripts/AnalyzeSong.cs
+++ Assets/Scripts/AnalyzeSong.cs
@@ -41,51 +41,23 @@ public class AnalyzeSong : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		spec = audio.GetSpectrumData(64,0,FFTWindow.Hamming);
+		spec = audio.GetSpectrumData(64,0,FFTWindow.Rectangular);
 #if DEBUG
         double dspTime = AudioSettings.dspTime;
 #endif
-        Debug.Log("Current time audio system: "+dspTime);
-//		spec = AudioListener.GetOutputData (64,0);  
-		/*
-	c1 = 64hz
-	c3 = 256hz
-	c4 = 512hz
-	*/
+//        Debug.Log("Current time audio system: "+ dspTime); 
+		/* c1 = 64hz c3 = 256hz c4 = 512hz */
 		c1 = spec[2] + spec[4] + spec[3];
 		c2 = spec[11] + spec[12] + spec[13];
 		c3 = spec[22] + spec[23] + spec[24];
 		c4 = spec[44] + spec[45] + spec[45] + spec[46] + spec[47] + spec[48];
-		
-//		cube01.gameObject.transform.localScale = new Vector3(20f,c1*juice,1f);
-//		cube02.gameObject.transform.localScale = new Vector3(20f,c2*juice,1f);
-//		cube03.gameObject.transform.localScale = new Vector3(20f,c3*juice,1f);
-//		cube04.gameObject.transform.localScale = new Vector3(20f,c4*juice,1f);
-
-
 
 		if (1000 * (c3+c4) > 0.1) {
-			Debug.Log (100 * (c3 + c4));
+//			Debug.Log (100 * (c3 + c4));
 
-//			light1.gameObject.SetActive (false);
-
-
-//			noteKey = Random.Range (0, 2);
-
-//			InvokeRepeating("SpawnNote", 0.0f, 2.0f);
-
-//			Invoke("SpawnNote", 0.1f);
-
-			SpawnNote();
+            int noteKey = Random.Range(0, 2);
+			SpawnNote(noteKey);
 			noteCount += 1;
-
-
-
-//			Debug.Log ("Note to hit =" + noteKey);
-
-
-
-//			StartCoroutine (DelaySpawn ());
 
 
 
@@ -96,31 +68,13 @@ public class AnalyzeSong : MonoBehaviour
 
 	}
 
-//	IEnumerator DelaySpawn()
-//	{
-//		float timeToWait = 5f;
-//
-//		yield return new WaitForSeconds (timeToWait);
-//		SpawnNote ();
-//
-//
-//	}
-
-	void SpawnNote(){
-
-        int noteKey = Random.Range(0, 2);
-
-		if (noteKey == 0){
-			//				Debug.Log(note.GetComponent<SpriteRenderer> ().sprite.ToString());
-			Instantiate(noteBlack, new Vector2(12, -2), Quaternion.identity);
-			
-			//				(Texture2D)LoadAssetAtPath("Assets/Art/Pixel/notes/note_black.png", typeof(Sprite)) as Sprite);
-			
-			//				note.GetComponent<SpriteRenderer> ().sprite = Resources.Load("Assets\\Art\\Pixel\\note_black", typeof(Sprite)) as Sprite;
+	void SpawnNote(int noteKey){
+        
+        //Debug.Log("Note key :" + noteKey);
+		if (noteKey == 0) {
+			Instantiate(noteBlack, new Vector2(12, -4), Quaternion.identity);
 		} else {
-			Instantiate(noteWhite, new Vector2(12, -4), Quaternion.identity);
-			
-			//				note.GetComponent<SpriteRenderer> ().sprite = Resources.Load("Assets\\Art\\Pixel\\notes", typeof(Sprite)) as Sprite;
+			Instantiate(noteWhite, new Vector2(12, -2), Quaternion.identity);
 		}
 	}
 	
