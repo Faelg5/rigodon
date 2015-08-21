18ee636225562ed90c7392b72594de44511930b9
GuillaumeDux <guillaume.dux@gmail.com>
2015-08-21 05:12:48 +0200

    UI tweaks

---
+++ Assets/Scripts/OpenSurprise.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenSurprise : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Application.LoadLevel ("Surprise");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void LoadLevel(){
+		Application.LoadLevel ("Surprise");
+	}
+
+}
